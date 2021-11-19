0df6fc2ebefe39b8de6977e7c1deec1f47f6ccb1
Bryan Reed <breed@yext.com>
2021-11-19 14:52:54 -0500

    unflatten

--- src/schemaparse.js
+++ src/schemaparse.js
@@ -1,8 +1,8 @@
 function schemaparse(schema) {
-    return parseFields(schema, "");
+    return parseFields(schema, "", "");
 }
 
-function parseFields(properties, namespace) {
+function parseFields(properties, namespace, displayNamespace) {
     var fields = [] 
     if (properties == null) {
         return fields;
@@ -10,10 +10,9 @@ function parseFields(properties, namespace) {
     for (const [key, value] of Object.entries(properties)) {
         var subfields = [];
         if (value.type != null && 'structType' in value.type) {
-            subfields = parseFields(value.type.structType.property, namespace + value.name + '.');
+            subfields = parseFields(value.type.structType.property, namespace + value.name + '.', displayNamespace + value.displayName + ' > ');
         }  
-        fields.push({name: namespace + value.name, displayName: value.displayName, type:value.typeId});
-        fields = fields.concat(subfields);
+        fields.push({name: namespace + value.name, displayName: value.displayName, type:value.typeId, subfields:subfields});
     }
     return fields;
 }
